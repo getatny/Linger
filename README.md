581d947fa1a6f7a2d600c76d6f8e52d0996bd41c
Getatny <wangmaozhu@foxmail.com>
2019-11-17 13:42:44 +0800

    更新README.md 更新：README.md 修复：安卓系统首页专辑处显示横向滚动条 TODO：文章页仍未完成

---
+++ README.md
@@ -0,0 +1,25 @@
+## Linger - 弥留
+
+`Linger - 弥留` 是一款推荐音乐、文字的微信小程序
+
+---
+
+## 程序截图
+
+<img src="https://s2.ax1x.com/2019/11/17/MD7YE4.jpg" style="margin-right: 2%;" width="30%" /><img src="https://s2.ax1x.com/2019/11/17/MD7tUJ.jpg" style="margin-right: 2%;" width="30%" /><img src="https://s2.ax1x.com/2019/11/17/MD7N59.png" style="" width="30%" />
+
+---
+
+## 如何运行
+
+该小程序使用 [Tarojs]( https://taro.aotu.io/ ) 开发，所以首先你得确保你电脑已经安装了`Node`。你可以使用`node -v`查看本机 Nodejs 版本，确定已安装Node之后，运行：
+
+```bash
+npm install && npm run dev:weapp
+```
+
+当然如果你使用yarn作为包管理器，也可以这样：
+
+```bash
+yarn install && yarn dev:weapp
+```
--- src/pages/article/article.js
+++ src/pages/article/article.js
@@ -1,4 +1,4 @@
-import { ScrollView, View } from "@tarojs/components"
+import { ScrollView, Swiper, SwiperItem, View } from "@tarojs/components"
 import Taro, { Component } from '@tarojs/taro'
 import TitleBar from "../../components/titleBar/titleBar";
 import TopCover from "../../components/topCover/topCover";
@@ -7,11 +7,24 @@ const ArticleCover = 'https://www.getatny.com/wp-content/uploads/2019/09/article
 
 export default class Article extends Component {
 
+    state = {
+        trendCategories: [
+            { name: '情感' },
+        ]
+    }
+
     render() {
         return <View className='article-page'>
             <ScrollView scrollY className='main-content'>
                 <TitleBar />
                 <TopCover cover={ArticleCover} logo />
+                <Swiper className='trend-categories'>
+                    {this.state.trendCategories.map(category => (
+                        <SwiperItem>
+                            <View>{category.name}</View>
+                        </SwiperItem>
+                    ))}
+                </Swiper>
             </ScrollView>
         </View>
     }
